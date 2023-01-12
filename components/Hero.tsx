0b14e0c14f26b007f0403b296a63b968b912a4ff
waywedesign <ajithdev.c@waywedesign.com>
2023-01-12 17:56:24 +0530

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <div>
+        <div className='ml-20 mt-24'>
+        <h2 className='text-2xl font-bold'>DIGITAL MARKETING COMPANY IN<br/>KERALA</h2>
+        </div>
+
+        <div>
+          <h1 className='text-3xl ml-20 mt-10'>GROWTH</h1>
+          <h2 className='text-4xl font-black ml-20 mt-5 text-green-400'>REDEFINED</h2>
+        </div>
+        <div>
+          <h3 className='text-xl leading-10 p-10 ml-10'>The epitome of dedication and perseverance, our team<br/>
+             focuses on the exponential growth of the clients through<br/>
+             tireless commitment.</h3>
+        </div>
+        <div className='text-xl ml-20 '>
+          <button className='text-green-600 rounded-full px-6 py-2 bg-gray-400 font-bold' >Contact US</button>
+        </div>
+
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ components/Navbar.tsx
@@ -0,0 +1,94 @@
+import Link from 'next/link'
+import React,{useState, useEffect} from 'react'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+
+const Navbar = () => {
+
+     const [nav, setNav] = useState(false)
+     const [color, setColor] = useState('transparent')
+     const [textColor, setTextColor] = useState('black')
+
+     const handleNav = () => {
+        setNav(!nav)
+     }
+
+     useEffect(() => {
+        const changeColor = () => {
+            if(window.scrollY >=90){
+                setColor('#ffffff')
+                setTextColor('#000000')
+            }else{
+                setColor('transparent')
+                setTextColor('#000000')
+            }
+        }
+        window.addEventListener('scroll', changeColor)
+     },[])
+  return (
+    <div style={{backgroundColor: 'white'}} className='fixed text-white left-0 top-0 w-full z-10 ease-in duration-300'>
+        <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 '>
+            <Link href='/'>
+                <h1 style={{color: `${textColor}`}} className='font-bold text-4xl'>DevArc</h1>
+            </Link>
+            <ul style={{color: `${textColor}`}} className='sm:flex gap-10 hidden '>
+                <li className='p-4'>
+                    <Link href='/digitalmarketing'>Digital Marketing</Link>
+                </li>
+                <li className='p-4'>
+                    <Link href='/webdevelopment'>Web Development</Link>
+                </li>
+                <li className='p-4'>
+                    <Link href='/branding'>Branding</Link>
+                </li>
+                <li className='p-4'>
+                    <Link href='itsolutions'>IT Solutions</Link>
+                </li>
+                <li className='p-4'>
+                    <Link href='company'>Company</Link>
+                </li>
+                <li className='p-4'>
+                    <Link href='blog'>Blog</Link>
+                </li>
+            </ul>
+
+            {/* mobileButton */}
+
+            <div onClick={handleNav} className='block sm:hidden z-[10] cursor-pointer'>
+                {nav?(
+                    <AiOutlineClose size={30} style={{color:`${textColor}`}}/>
+                ) :(
+                    <AiOutlineMenu size={30} style={{color: `${textColor}`}}/>
+                )}
+            </div>
+
+            {/* mobilemenu */}
+
+            <div className={nav ? 'absolute left-0 top-6 right-0 bottom-0 w-full justify-center items-center h-screen text-black bg-white cursor-pointer text-center ease-in duration-300' : 
+            'absolute top-6 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen text-center ease-in duration-300'}>
+                <ul>
+                    <li onClick={handleNav} className='p-4 text-3xl hover:text-gray-500'>
+                        <Link href='/digitalmarketing'>Digital Marketing</Link>
+                    </li>
+                    <li onClick={handleNav} className='p-4 text-3xl hover:text-gray-500'>
+                        <Link href='/webdevelopment'>Web Development</Link>
+                    </li>
+                    <li onClick={handleNav} className='p-4 text-3xl hover:text-gray-500'>
+                        <Link href='/branding'>Branding</Link>
+                    </li>
+                    <li onClick={handleNav} className='p-4 text-3xl hover:text-gray-500'>
+                        <Link href='/itsolutions'>IT Solutions</Link>
+                    </li>
+                    <li onClick={handleNav} className='p-4 text-3xl hover:text-gray-500'>
+                        <Link href='/company'>Company</Link>
+                    </li>
+                    <li onClick={handleNav} className='p-4 text-3xl hover:text-gray-500'>
+                        <Link href='/blog'>Blog</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
